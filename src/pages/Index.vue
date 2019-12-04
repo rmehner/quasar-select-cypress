a5e485e78ee2d72b6c324b50fc9383b29cbfe333
Robin Mehner <robin@coding-robin.de>
2019-12-04 15:55:04 +0100

    Add a progress bar to show that nextTick works as expected and set virtual-scroll-item-size to show bug. And add scroll handler for debugging purposes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <q-select outlined label="Number" :options="options" />
+    <q-linear-progress :value="progress" />
+    <q-select
+      outlined
+      label="Number"
+      :options="options"
+      v-model="selectedValue"
+      @virtual-scroll="onScroll"
+      :virtual-scroll-item-size="scrollItemSize"
+    />
   </q-page>
 </template>
 
@@ -9,8 +17,31 @@ export default {
   name: "PageIndex",
   data() {
     return {
-      options: [...Array(5000).keys()]
+      progress: 0.01,
+      selectedValue: "",
+      options: [...Array(100).keys()],
+      // changing the following to 0 makes it work with Cypress
+      scrollItemSize: 24
     };
+  },
+
+  mounted() {
+    setInterval(() => {
+      this.$nextTick(() => {
+        if (this.progress >= 1) {
+          this.progress = 0.01;
+          return;
+        }
+
+        this.progress = Math.min(1, this.progress + 0.1);
+      });
+    }, 700);
+  },
+
+  methods: {
+    onScroll(details) {
+      console.log(details);
+    }
   }
 };
 </script>
